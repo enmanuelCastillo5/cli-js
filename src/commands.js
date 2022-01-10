cdad90a4153d9526ec6c3a2c3363ea093ff9222f
enmanuelCastillo5 <enmanuel.castillo@hotmail.com>
2022-01-10 15:54:17 -0400

    reciving Data

--- src/commands.js
+++ src/commands.js
@@ -1,3 +1,20 @@
 const { program } = require('commander');
+const { prompt } = require('inquirer')
 program.version('0.0.1').description('A command line tool for managing Tasks');
+
+program.command('save ').action( async () => {
+   const answer = await prompt([
+        {
+            type: 'input',
+            message: 'task Title',
+            name: 'title'
+        },
+        {
+            type: 'input',
+            message: 'task Description',
+            name: 'description'
+        }
+    ])
+    console.log(answer)
+});
 program.parse(process.argv);
