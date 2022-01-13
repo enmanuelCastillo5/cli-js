9ef3a9e7fede0c77dca9fee4df3d9a8f7f4a4bd1
enmanuelCastillo5 <enmanuel.castillo@hotmail.com>
2022-01-13 13:45:53 -0400

    updat Task and find text

--- src/commands.js
+++ src/commands.js
@@ -1,7 +1,7 @@
 const { program } = require('commander');
 const { prompt } = require('inquirer')
 
-const { addTask, ListTask, DeleteTask } = require('./controllers/taskControllers')
+const { addTask, ListTask, ListTaskId, DeleteTask, updateTask, findTask } = require('./controllers/taskControllers')
 
 program.version('0.0.1').description('A command line tool for managing Tasks');
 
@@ -28,7 +28,20 @@ program.command('list').alias('l').action( async () => {
     ListTask()
 })
 
+program.command('listId <id>').alias('lid').action( async (_id) => {
+    ListTaskId(_id)
+})
+
+program.command('find <task>').alias('f').action( async (text) => {
+    findTask(text)
+})
+
 program.command('del <id>').alias('d').action( async (_id) => {
     DeleteTask(_id)
+})
+
+program.command('update <id>').alias('u').action( async (_id) => {
+    const answer = await prompt(taskQuestions)
+    await updateTask(_id, answer)
 })
 program.parse(process.argv);
--- src/controllers/taskControllers.js
+++ src/controllers/taskControllers.js
@@ -8,6 +8,25 @@ const addTask = async (task) => {
     await connection.close()
 }
 
+const findTask = async (text) => {
+    const search = new RegExp(text, 'i')
+    const tasks = await Task.find({
+        $or: [{title: search}, {description: search}]
+    })
+    if(tasks.length == 0){
+        console.log('tasks not found')
+        await connection.close()
+        process.exit(0)
+    }
+    console.table({
+        id: tasks[0]._id.toString(),
+        title: tasks[0].title,
+        description: tasks[0].description
+    })
+    await connection.close()
+    process.exit(0)
+}
+
 const ListTask = async () => {
     const tasks = await Task.find().lean()
     console.table(tasks.map(task => ({
@@ -23,9 +42,27 @@ const DeleteTask = async (_id) => {
     console.log(`Delete: ${_id}`)
     await connection.close()
 }
+const ListTaskId = async (_id) => {
+    const taskId = await Task.findById(_id).lean()
+    console.table({
+        _id: taskId._id.toString(),
+        title: taskId.title,
+        description: taskId.description
+    })
+    await connection.close()
+}
+
+const updateTask = async (_id, newTask) => {
+   await Task.updateOne({_id}, newTask)
+    console.log(`Task Updated: ${_id}}`)
+    await connection.close()
+}
 
 module.exports = {
     addTask,
     ListTask,
-    DeleteTask
+    DeleteTask,
+    ListTaskId,
+    updateTask,
+    findTask
 }
